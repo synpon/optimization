a55731bffae6a994779f166e5d6ee739896a3653
Ubuntu <ubuntu@ip-172-31-21-243.eu-west-1.compute.internal>
2016-07-12 20:53:18 +0000

    Optimizer runs for the first time

--- optimizer.py
+++ optimizer.py
@@ -11,173 +11,212 @@ Loss: Change in loss
 
 summary_freq = 100
 summaries_dir = '/tmp/logs'
-batch_size = 32
-epochs = 100
+test_evaluation = False
 #rnn = False # feed-forward otherwise
 #rnn_num_layers = 1
 #rnn_size = 5
-#rnn_seq_length = 5
+seq_length = 1
+epochs = 1
 
 grad_scaling_methods = ['scalar','full']
 grad_scaling_method = grad_scaling_methods[0]
 grad_scaling_factor = 0.1
 
+
+class MLP:
+	def __init__(self):
+		self.batch_size = 128
+		self.batches = 1000
+
+		# Define architecture
+		self.x = tf.placeholder(tf.float32, [None, 784])
+		self.y_ = tf.placeholder(tf.float32, [None,10])
+
+		self.W = tf.Variable(tf.zeros([784, 10]))
+		self.b = tf.Variable(tf.zeros([10]))
+		y = tf.nn.softmax(tf.matmul(self.x,self.W) + self.b)
+		
+		correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(self.y_,1))
+		self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+		self.loss = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(y), reduction_indices=[1])) # Cross-entropy
+
+		optimizer = tf.train.GradientDescentOptimizer(0.5)
+		
+		grad_var_pairs = optimizer.compute_gradients(self.loss)
+		grads,vars = zip(*grad_var_pairs)
+		grads = [tf.reshape(i,(-1,1)) for i in grads]
+		self.grads = tf.concat(0,grads)
+		
+		self.params = tf.trainable_variables()
+		
+		self.train_step = optimizer.apply_gradients(grad_var_pairs)
+
+		self.trainable_variables = tf.trainable_variables()
+		self.init = tf.initialize_all_variables()
+
+		
+class OptNet:
+	def __init__(self):
+		batches = 1000
+		batch_size = 1 ###
+
+		# Define architecture
+		# Feed-forward
+		if grad_scaling_method == 'scalar':
+			self.x_grads = tf.placeholder(tf.float32, [None,None,1])
+		elif grad_scaling_method == 'full':
+			self.x_grads = tf.placeholder(tf.float32, [None,None,2])
+
+		self.y_losses = tf.placeholder(tf.float32) ### batch?
+		
+		# Scale inputs
+		self.scale = tf.placeholder(tf.float32)
+		self.p = tf.placeholder(tf.float32)
+
+		grad_threshold = tf.exp(-self.p)
+
+		if grad_scaling_method == 'scalar':
+			x = self.x_grads*self.scale
+			
+		elif grad_scaling_method == 'full':
+			# Operations are element-wise
+			mask = tf.greater(self.x_grads,grad_threshold)
+			mask = tf.to_float(mask) # Convert from boolean
+			inv_mask = 1 - mask
+			
+			x1_cond1 = tf.log(tf.abs(self.x_grads))/self.p
+			x2_cond1 = tf.sign(self.x_grads)
+			x1_cond2 = -tf.ones(tf.shape(self.x_grads))
+			x2_cond2 = tf.exp(self.p)*self.x_grads
+			
+			x1 = x1_cond1*mask + x1_cond2*inv_mask
+			x2 = x2_cond1*mask + x2_cond2*inv_mask
+			
+			x = tf.concat(2,[x1,x2])
+			
+		if grad_scaling_method == 'scalar':		
+			W1 = tf.Variable(tf.zeros([1,4]))
+		elif grad_scaling_factor == 'full':
+			W1 = tf.Variable(tf.zeros([2,4]))
+			
+		b1 = tf.Variable(tf.zeros([4]))
+
+		W1_1 = tf.reshape(W1,(1,-1,4)) # Convert from rank 2 to rank 3
+		W1_1 = tf.tile(W1_1,(batch_size,1,1))
+
+		h = tf.nn.relu(tf.batch_matmul(x,W1_1) + b1)
+		
+		W2 = tf.Variable(tf.zeros([4,1]))		
+		b2 = tf.Variable(tf.zeros([1]))
+
+		W2_1 = tf.reshape(W2,(1,-1,1)) # Convert from rank 2 to rank 3
+		W2_1 = tf.tile(W2_1,(batch_size,1,1))
+		
+		h = tf.nn.relu(tf.batch_matmul(h,W2_1) + b2) # Gradients
+		
+		# Apply gradients to the parameters in the train net.
+		total = 0
+		for i,v in enumerate(mlp.trainable_variables):
+			size = np.prod(list(v.get_shape()))
+			size = tf.to_int32(size)
+			var_grads = tf.slice(h,begin=[0,total,0],size=[-1,size,-1]) ### Account for the other dimensions
+			var_grads = tf.reshape(var_grads,v.get_shape())
+			v.assign_add(var_grads)
+			size += total
+		
+		# Calculate loss for the updated train net.
+		#_, new_loss = sess.run([mlp.train_step,mlp.loss], feed_dict={mlp.x: batch_x, mlp.y_: batch_y})
+		
+		self.x = tf.placeholder(tf.float32, [None,784])
+		self.y_ = tf.placeholder(tf.float32, [None,10])
+
+		y = tf.nn.softmax(tf.matmul(self.x,mlp.W) + mlp.b)
+		new_loss = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(y), reduction_indices=[1])) # Cross-entropy	
+		
+		# Change in loss as a result of the parameter update (ideally negative)
+		self.loss = new_loss - self.y_losses
+		
+		optimizer = tf.train.AdamOptimizer()
+		self.train_step = optimizer.minimize(self.loss)
+
+		self.init = tf.initialize_all_variables()
+		
+		
+sess = tf.Session()		
+		
+mlp = MLP()
+sess.run(mlp.init)
+
+train_grads_list = []
+train_losses_list = []
+train_params_list = []
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-train_grads = []
-train_losses = []
-train_params = []
-
-##### Generate training data #####
-with tf.variable_scope("mlp"):
-	batch_size = 128
-	batches = 1000
-
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-	# Define architecture
-	x = tf.placeholder(tf.float32, [None, 784])
-
-	W = tf.Variable(tf.zeros([784, 10]))
-	b = tf.Variable(tf.zeros([10]))
-	y = tf.nn.softmax(tf.matmul(x,W) + b)
-
-	y_ = tf.placeholder(tf.float32, [None,10])
-	loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1])) # Cross-entropy
-
-	optimizer = tf.train.GradientDescentOptimizer(0.5)
+for i in range(mlp.batches):
+	batch_x, batch_y = mnist.train.next_batch(mlp.batch_size)
+	# Train the batch, remembering the gradients and the loss
+	tmp = sess.run([mlp.train_step,mlp.loss,mlp.grads] + mlp.params, feed_dict={mlp.x: batch_x, mlp.y_: batch_y})
+	loss_,grads_ = tmp[1:3]
+	params_ = tmp[3:]
 	
-	grad_var_pairs = optimizer.compute_gradients(loss)
-	grads,vars = zip(*grad_var_pairs)
-	grads = [tf.reshape(i,(-1,1)) for i in grads]
-	grads = tf.concat(0,grads)
+	train_losses_list.append(loss_)
+	train_grads_list.append(grads_)
+	train_params_list.append(params_)
 	
-	params = tf.trainable_variables()
-	
-	train_step = optimizer.apply_gradients(grad_var_pairs)
+print(sess.run(mlp.accuracy, feed_dict={mlp.x: mnist.test.images, mlp.y_: mnist.test.labels}))
 
-	init = tf.initialize_all_variables()
+opt_net = OptNet()
+sess.run(opt_net.init)
 
-	sess = tf.Session()
-	sess.run(init)
+sess.run(mlp.init) # reset variables for safety
 
-	for i in range(batches):
-		batch_x, batch_y = mnist.train.next_batch(batch_size)
-		# Train the batch, remembering the gradients and the loss
-		tmp = sess.run([train_step,loss,grads] + params, feed_dict={x: batch_x, y_: batch_y})
-		loss_,grads_ = tmp[1:3]
-		params_ = tmp[3:]
+for epoch in range(epochs):
+	print "Epoch ", epoch
+
+	# Leave room for the rest of the sequence at the end
+	max_length = len(train_grads_list) - seq_length
+	perm = np.random.permutation(range(max_length))
+
+	for i in range(max_length):
+		start = perm[i]
+		end = start + seq_length
+		train_grads_seq = train_grads_list[start:end]
+		train_params = train_params_list[end]
+		train_loss = train_losses_list[end]
 		
-		train_losses.append(loss_)
-		train_grads.append(grads_)
-		train_params.append(params_)
+		# Used to compute the change in the loss
+		batch_x, batch_y = mnist.train.next_batch(mlp.batch_size)
 		
-	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-	
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-	print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
-	
-print "\nTraining data generated"
-print len(train_losses)
-
-##### Train network #####
-with tf.variable_scope("opt_net"):
-
-	batches = 1000
-
-	# Define architecture
-	# Feed-forward
-	x_grads = tf.placeholder(tf.float32, [None,None,2])
-	y_loss = tf.placeholder(tf.float32, [None])
-	
-	# Scale inputs
-	scale = tf.placeholder(tf.float32)
-	p = tf.placeholder(tf.float32)
-
-	grad_threshold = tf.exp(-p)
-
-	if grad_scaling_method == 'scalar':
-		x = x_grads*scale
+		# Set params
+		for j,v in enumerate(mlp.trainable_variables):
+			v.assign(train_params[j])
 		
-	elif grad_scaling_method == 'full':
-		# Operations are element-wise
-		mask = tf.greater(x_grads,grad_threshold)
-		mask = tf.to_float(mask) # Convert from boolean
-		inv_mask = 1 - mask
+		### Do in batches
+		# Train the optimizer
+		_,loss_ = sess.run([opt_net.train_step,opt_net.loss], 
+							feed_dict={	opt_net.x_grads: train_grads_seq,
+										opt_net.y_losses: train_loss,
+										opt_net.x: batch_x,
+										opt_net.y_: batch_y})
 		
-		x1_cond1 = tf.log(tf.abs(x_grads))/p
-		x2_cond1 = tf.sign(x_grads)
-		x1_cond2 = -tf.ones(tf.shape(x_grads))
-		x2_cond2 = tf.exp(p)*x_grads
-		
-		x1 = x1_cond1*mask + x1_cond2*inv_mask
-		x2 = x2_cond1*mask + x2_cond2*inv_mask
-		
-		x = tf.concat(2,[x1,x2])
-		
-	if grad_scaling_method == 'scalar':		
-		W1 = tf.Variable(tf.zeros([1,4]))
-	elif grad_scaling_factor == 'full':
-		W1 = tf.Variable(tf.zeros([2,4]))
-		
-	b1 = tf.Variable(tf.zeros([4]))
-
-	W1_1 = tf.reshape(W1,(1,-1,4)) # Convert from rank 2 to rank 3
-	W1_1 = tf.tile(W1_1,(batch_size,1,1))
-
-	h = tf.nn.relu(tf.batch_matmul(x,W1_1) + b1)
-	
-	W2 = tf.Variable(tf.zeros([4,1]))		
-	b2 = tf.Variable(tf.zeros([1]))
-
-	W2_1 = tf.reshape(W2,(1,-1,1)) # Convert from rank 2 to rank 3
-	W2_1 = tf.tile(W2_1,(batch_size,1,1))
-	
-	h = tf.nn.relu(tf.batch_matmul(h,W2_1) + b2)
-	
-	### Make more efficient by using it in the style of AdamOptimizer etc.	
-	# Apply gradients to the parameters in the train net.
-	
-	
-	# Calculate loss for the updated train net.
-	
-	
-	# Change in loss as a result of the parameter update
-	loss =  - y_loss
-	
-	optimizer = tf.train.AdamOptimizer()
-	train_step = optimizer.minimize(loss)
-
-	init = tf.initialize_all_variables()
-
-	sess = tf.Session()
-	sess.run(init)
-	
-	for i in range(epochs):
-		print "Epoch ", i
-		### Needs to be adapted for sequences
-		perm = np.random.permutation(range(len(train_grads)))
-		shuffled_train_grads = train_grads[perm]
-		shuffled_train_losses = train_losses[perm]
-		shuffled_train_params = train_params[perm]
-		
-		for start in range(0, len(train_grads), batch_size):
-			end = start + batch_size
-			train_grads_batch = shuffled_train_grads[start:end]
-			train_params_batch = shuffled_train_params[start:end]
-			train_losses_batch = shuffled_train_losses[start:end]
-			
-			_,loss_ = sess.run([train_step,loss], feed_dict={train_grads_batch,train_params_batch,train_losses_batch})
-			if i % summary_freq == 0:
-				print loss_
-
+		if i % summary_freq == 0:
+			print loss_, i
 
 ##### Compare optimizer performance using TensorBoard #####
-if tf.gfile.Exists(summaries_dir):
-	tf.gfile.DeleteRecursively(summaries_dir)
-tf.gfile.MakeDirs(summaries_dir)
+if test_evaluation:
+	if tf.gfile.Exists(summaries_dir):
+		tf.gfile.DeleteRecursively(summaries_dir)
+	tf.gfile.MakeDirs(summaries_dir)
 
-sgd_writer = tf.train.SummaryWriter(summaries_dir + '/sgd')
-adam_writer = tf.train.SummaryWriter(summaries_dir + '/adam')
-opt_net_writer = tf.train.SummaryWriter(summaries_dir + '/opt_net')
+	#sgd_writer = tf.train.SummaryWriter(summaries_dir + '/sgd')
+	adam_writer = tf.train.SummaryWriter(summaries_dir + '/adam')
+	opt_net_writer = tf.train.SummaryWriter(summaries_dir + '/opt_net')
+
+	# Optimizer parameters are now treated as fixed
+
+	sess.run(mlp.init)
+	
+	sess.run(mlp.init)
 
