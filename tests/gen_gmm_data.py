9dd84b8bf0e7d50349ad9e3e19e717df9a2275d9
Ubuntu <ubuntu@ip-172-31-21-243.eu-west-1.compute.internal>
2016-07-20 00:10:49 +0000

    Exponentiation works in the GMM

--- tests/gen_gmm_data.py
+++ tests/gen_gmm_data.py
@@ -4,9 +4,9 @@ import numpy as np
 # Generates n points and their losses from one landscape
 # Creating a sufficient training dataset would require this to be run multiple times
 
-num_gaussians = 1 # Number of Gaussians
-m = 1 # Number of dimensions
-n = 10 # Training set size, number of points
+num_gaussians = 100 # Number of Gaussians
+m = 10 # Number of dimensions
+n = 1000 # Training set size, number of points
 
 # Not probabilities so normalization is not necessary
 gaussian_weights = tf.Variable(tf.random_uniform(shape=(num_gaussians,)))
@@ -28,15 +28,17 @@ points = tf.Variable(tf.random_uniform(shape=(n,m,1),dtype=tf.float32))
 # Compute the distance for every point from every mean vector
 tmp_points = tf.tile(points, multiples=[1,1,num_gaussians])
 tmp_mean_vectors = tf.tile(mean_vectors, multiples=[1,1,n]) 
-d = tmp_points - tf.transpose(tmp_mean_vectors,[2,1,0]) # (n,m,num_gaussians)
+d = tmp_points - tf.transpose(tmp_mean_vectors,[2,1,0]) # n,m,num_gaussians
 
-inv_cov_matrices = tf.batch_matrix_inverse(cov_matrices)
+inv_cov_matrices = tf.batch_matrix_inverse(cov_matrices) # num_gaussians,m,m
 
-# The pdfs of the Gaussians are negative in order to create a minimization problem.
 losses = tf.batch_matmul(tf.transpose(d,[2,0,1]),inv_cov_matrices)
-losses = tf.batch_matmul(losses,tf.transpose(d,[2,1,0]))
+# Follows the code in SciPy's multivariate_normal
+losses = tf.square(losses) # element-wise (num_gaussians,n,m)
+losses = tf.reduce_sum(losses,[2]) # Sum over the dimensions
+# The pdfs of the Gaussians are negative in order to create a minimization problem.
 losses = -tf.exp(-0.5*losses)
-losses = tf.reduce_mean(losses,[0,1]) # Sum over the Gaussians and dimensions
+losses = tf.reduce_mean(losses,[0]) # Average over the Gaussians
 
 opt = tf.train.GradientDescentOptimizer(0.5) # Only used to compute gradients, not optimize
 # Computes gradients for all variables. Only the gradients for 'points' are needed
