Scaling added. Minor fixes.

v2/a3c.py
@@ -8,6 +8,7 @@ import math
 import os
 import threading
 import signal
+import argparse
 
 from ac_network import A3CRNN, A3CFF
 from a3c_training_thread import A3CTrainingthread
@@ -18,6 +19,12 @@ from constants import num_threads, initial_alpha_low, \
 	initial_alpha_high, initial_alpha_log_rate, max_time_steps, \
 	log_file, rmsp_epsilon, rmsp_alpha, grad_norm_clip, \
 	use_rnn, summary_freq, save_path
+
+	
+parser = argparse.ArgumentParser()
+parser.add_argument('--save', '-s', dest='save_model', action='store_true')
+parser.set_defaults(save=False)
+args = parser.parse_args()
 
 	
 # Globals
@@ -106,7 +113,8 @@ with graph.as_default(), tf.Session() as sess:
 		t.join()
 
 	# Save model
-	saver = tf.train.Saver(tf.trainable_variables())
-	saver.save(sess, save_path)
-	print "Model saved"
+	if args.save_model:
+		saver = tf.train.Saver(tf.trainable_variables())
+		saver.save(sess, save_path)
+		print "Model saved"
 	

v2/ac_network.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import rnn_cell
 
-from constants import rnn_size, num_rnn_layers, num_steps, m, rnn_type
+from constants import rnn_size, num_rnn_layers, num_steps, m, rnn_type, grad_scaling_method, grad_scaling_factor, p
 
 
 # Actor-Critic Network (policy and value network)
@@ -24,7 +24,7 @@ class A3CNet(object):
 		# Minus because this is for gradient ascent
 		# Overlap between the distributions
 		policy_loss = -(tf.nn.l2_loss(self.mean - self.a) * self.td + entropy*entropy_beta) ### Confirm use of L2 loss
-
+		### Print relative magnitudes, including the value loss
 		# R (input for value)
 		self.r = tf.placeholder(tf.float32, [1], 'r')
 
@@ -35,7 +35,7 @@ class A3CNet(object):
 		
 
 	def sync_from(self, src_network, name=None):
-		src_vars = src_network.trainable_vars ### built-in function to do this?
+		src_vars = src_network.trainable_vars
 		dst_vars = self.trainable_vars
 
 		sync_ops = []
@@ -48,11 +48,13 @@ class A3CNet(object):
 
 	def weight_matrix(self, n_in, n_out):
 		with tf.variable_scope("weight"):
-			return tf.Variable(tf.truncated_normal(stddev=0.1, shape=[1, n_in, n_out]))
+			d = 1.0/np.sqrt(n_in)
+			return tf.Variable(tf.random_uniform(shape=[1, n_in, n_out], minval=-d, maxval=d))
 	
-	def bias_vector(self,n_out):
+	def bias_vector(self, n_in, n_out):
 		with tf.variable_scope("bias"):
-			return tf.Variable(tf.constant(0.1, shape=[n_out]))
+			d = 1.0/np.sqrt(n_in)
+			return tf.Variable(tf.random_uniform(shape=[n_out], minval=-d, maxval=d))
 
 	# Update the parameters of another network (eg an MLP)
 	def update_params(self, vars, h):
@@ -69,10 +71,54 @@ class A3CNet(object):
 			#	var_grads = tf.clip_by_value(var_grads, -grad_clip_value, grad_clip_value)
 			
 			ret.append(v.assign_add(var_grads))
-			size += total
-			
+			size += total		
 		return tf.group(*ret)
-	
+
+		
+	def scale_grads(self,input):
+		if grad_scaling_method == 'scalar':
+			return input*tf.constant(grad_scaling_factor)	
+			
+		elif grad_scaling_method == 'full':	
+			p_ = tf.constant(p)
+			grad_threshold = tf.exp(-p_)
+		
+			# Operations are element-wise
+			mask = tf.greater(tf.abs(input),grad_threshold)
+			mask = tf.to_float(mask) # Convert from boolean
+			inv_mask = 1 - mask
+			
+			x1_cond1 = tf.log(tf.abs(input))/p_
+			x2_cond1 = tf.sign(input)
+			x1_cond2 = -tf.ones(tf.shape(input))
+			x2_cond2 = tf.exp(p_)*input
+			
+			x1 = x1_cond1*mask + x1_cond2*inv_mask
+			x2 = x2_cond1*mask + x2_cond2*inv_mask
+			
+			return tf.concat(2,[x1,x2])	
+		return input
+		
+		
+	def inv_scale_grads(self,input): ### use this
+		if grad_scaling_method == 'scalar':
+			return input/tf.constant(grad_scaling_factor)	
+			
+		elif grad_scaling_method == 'full':	
+			p_ = tf.constant(p)
+		
+			# Operations are element-wise
+			a,b = tf.split(2,2,input)
+			mask = tf.equal(tf.abs(b),1.0)
+			mask = tf.to_float(mask) # Convert from boolean
+			inv_mask = 1 - mask
+			
+			x_cond1 = tf.sign(b)*tf.exp(a*p_)
+			x_cond2 = b/tf.exp(p_)
+			
+			return x_cond1*mask + x_cond2*inv_mask		
+		return input
+		
 
 class A3CRNN(A3CNet):
 	# For an RNN, input is usually of shape [batch_size,num_steps]
@@ -82,36 +128,45 @@ class A3CRNN(A3CNet):
 		# Input (not the cell state)
 		batch_size = 1
 		self.state = tf.placeholder(tf.float32, [batch_size,m,1])
+		
+		state = self.scale_grads(self.state)
 
 		self.W1 = self.weight_matrix(rnn_size,1)
 		self.W1 = tf.tile(self.W1,(batch_size,1,1))
-		self.b1 = self.bias_vector(1)
+		self.b1 = self.bias_vector(1,1)
 		
 		self.W2 = self.weight_matrix(rnn_size,1)
 		self.W2 = tf.tile(self.W2,(batch_size,1,1))
-		self.b2 = self.bias_vector(1)
+		self.b2 = self.bias_vector(1,1)
 		
 		# Weights for value output layer
 		self.W3 = self.weight_matrix(rnn_size,1)
 		self.W3 = tf.tile(self.W3,(batch_size,1,1))
-		self.b3 = self.bias_vector(1)
+		self.b3 = self.bias_vector(1,1)
 		
-		#cell = rnn_cell.BasicLSTMCell(rnn_size)
-		cell = rnn_cell.BasicRNNCell(rnn_size)
+		if rnn_type == 'rnn':
+			self.cell = rnn_cell.BasicRNNCell(rnn_size)
+		elif rnn_type == 'gru':
+			self.cell = rnn_cell.GRUCell(rnn_size)
+		elif rnn_type == 'lstm':
+			self.cell = rnn_cell.BasicLSTMCell(rnn_size)
 
 		self.rnn_state = tf.zeros([1,m,rnn_size])
 		
 		if rnn_type in ['rnn','gru']:
-			self.rnn_state = self.state #[self.state,self.state]
+			self.rnn_state = state #[self.state,self.state]
 		elif rnn_type in ['lstm']:
 			raise NotImplementedError
 		
-		output, rnn_state_out = cell(self.state, self.rnn_state)
+		output,rnn_state_out = self.cell(state, self.rnn_state)
 		self.rnn_state_out = rnn_state_out
 	
 		# policy
 		self.mean = tf.batch_matmul(output, self.W1) + self.b1
 		self.variance = tf.nn.softplus(tf.batch_matmul(output, self.W2) + self.b2)
+		
+		#self.mean = self.inv_scale_grads(self.mean)
+		#self.variance = self.inv_scale_grads(self.variance)
 		
 		# value - linear output layer
 		self.v = tf.batch_matmul(output, self.W3) + self.b3
@@ -145,30 +200,32 @@ class A3CFF(A3CNet):
 		# Input
 		batch_size = 1
 		self.state = tf.placeholder(tf.float32, [batch_size,m,1])
+		
+		state = self.scale_grads(self.state) ### Add inverse scaling
 
 		with tf.variable_scope("A3CNet"):
 			with tf.variable_scope("policy_mean"):
 				self.W1 = self.weight_matrix(1,1)
 				self.W1 = tf.tile(self.W1,(batch_size,1,1))
-				self.b1 = self.bias_vector(1)
+				self.b1 = self.bias_vector(1,1)
 			
 			with tf.variable_scope("policy_variance"):
 				self.W2 = self.weight_matrix(1,1)
 				self.W2 = tf.tile(self.W2,(batch_size,1,1))
-				self.b2 = self.bias_vector(1)
+				self.b2 = self.bias_vector(1,1)
 			
 			# weights for value output layer
 			with tf.variable_scope("value"):
 				self.W3 = self.weight_matrix(1,1)
 				self.W3 = tf.tile(self.W3,(batch_size,1,1))
-				self.b3 = self.bias_vector(1)
+				self.b3 = self.bias_vector(1,1)
 
 		# policy
-		self.mean = tf.batch_matmul(self.state, self.W1) + self.b1
-		self.variance = tf.nn.softplus(tf.batch_matmul(self.state, self.W2) + self.b2)
+		self.mean = tf.batch_matmul(state, self.W1) + self.b1
+		self.variance = tf.nn.softplus(tf.batch_matmul(state, self.W2) + self.b2)
 		
 		# value - linear output layer
-		self.v = tf.batch_matmul(self.state, self.W3) + self.b3
+		self.v = tf.batch_matmul(state, self.W3) + self.b3
 		
 		if num_trainable_vars[0] == None:
 			num_trainable_vars[0] = len(tf.trainable_variables())

v2/compare.py
@@ -7,7 +7,10 @@ from constants import use_rnn, summaries_dir, save_path
 
 sess = tf.Session()
 
-opt_net = A3CFF([None])
+if use_rnn:
+	opt_net = A3CRNN([None])
+else:
+	opt_net = A3CFF([None])
 		
 # Load model
 saver = tf.train.Saver(tf.trainable_variables())

v2/constants.py
@@ -9,27 +9,36 @@ summary_freq = 100
 ##### A3C constants #####
 num_threads = 8
 local_t_max = 5 # repeat step size
-num_steps = min(5,local_t_max) # Number of steps to go back for truncated backprop.
+num_steps = 3 # Number of steps to go back for truncated backprop.
 rmsp_alpha = 0.99 # decay parameter for RMSProp
 rmsp_epsilon = 0.1 # epsilon parameter for RMSProp
-initial_alpha_low = 1e-4   # log_uniform low limit for learning rate 1e-4
-initial_alpha_high = 1e-4   # log_uniform high limit for learning rate 1e-2
+initial_alpha_low = 1e-5    # log_uniform low limit for learning rate
+initial_alpha_high = 1e-5   # log_uniform high limit for learning rate
 initial_alpha_log_rate = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4) ### description is incorrect
 entropy_beta = 1e-4 # entropy regularization constant 0.0001
-max_time_steps = 5e5
+max_time_steps = 5e7
 grad_norm_clip = 40.0 # gradient norm clipping
 discount_rate = 0.99
 
 ##### Opt net constants #####
-use_rnn = False # Uses a feed-forward network if false
+use_rnn = True # Uses a feed-forward network if false
 rnn_types = ['rnn','gru','lstm']
 rnn_type = rnn_types[1]
-rnn_size = 4 ### Causes W1 to be the wrong size?
+rnn_size = 2 ### Causes W1 to be the wrong size?
 num_rnn_layers = 1
 
 ##### GMM constants #####
 num_gaussians = 10 # 50 # Number of Gaussians
-m = 2 # 10 # Number of dimensions
+m = 4 # 10 # Number of dimensions
 cov_range = [0,2] # 16 ### Only the upper bound is used
 cov_range[1] *= np.sqrt(m)
 weight_gaussians = False
+
+grad_scaling_methods = ['none','scalar','full']
+grad_scaling_method = grad_scaling_methods[0]
+grad_scaling_factor = 0.1
+p = 10.0
+
+# Random noise is computed each time the point is processed while training the opt net
+#loss_noise = False
+#loss_noise_size = 0.2 # Determines the size of the standard deviation. The mean is zero.

v2/mlp.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from constants import use_rnn, rnn_type
 
 class MLP:
 	def __init__(self, opt_net, sess):
@@ -47,7 +48,12 @@ class MLP:
 		input = self.grads
 		input = tf.reshape(input,[1,-1,1]) ### check
 
-		updates = tf.batch_matmul(input, opt_net.W1) + opt_net.b1
+		if use_rnn:
+			output, rnn_state_out = opt_net.cell(input, opt_net.rnn_state)
+			opt_net.rnn_state = rnn_state_out
+			updates = tf.batch_matmul(output, opt_net.W1) + opt_net.b1
+		else: # Feedforward
+			updates = tf.batch_matmul(input, opt_net.W1) + opt_net.b1
 		
 		# Apply updates to the parameters in the train net.
 		self.opt_net_train_step = opt_net.update_params(self.trainable_variables, updates)