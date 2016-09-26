c00b6bb0c9e3cc2538c025291bc01addcf2f5059
Ubuntu <ubuntu@ip-172-31-21-243.eu-west-1.compute.internal>
2016-09-26 21:44:52 +0000

    Correlation causing increasing loss removed via separate optimizers.

--- v3/constants.py
+++ v3/constants.py
@@ -5,7 +5,7 @@ import numpy as np
 #===# Logging constants #===#
 summaries_dir = '/tmp/logs'
 save_path = 'models/model.ckpt'
-summary_freq = 50
+summary_freq = 1000
 
 #===# Opt net constants #===#
 rnn_types = ['rnn','gru']
@@ -20,17 +20,17 @@ var_size = 0.2
 
 #===# Training constants #===#
 batch_size = 32 ### use
-seq_length = 10
-num_iterations = 500000
+seq_length = 10 ### unused
+num_iterations = 100000
 num_SNFs = 100
-replay_mem_start_size = 1000 # DQN: 50000
-replay_memory_max_size = 10000 # DQN: 1000000
+replay_mem_start_size = 5000 # DQN: 50000
+replay_memory_max_size = 100000 # DQN: 1000000
 episode_length = 1000 # DQN: 1000000, 250000?
 discount_rate = 0.999
-net_update_freq = 1000 # DQN: 10000
+net_sync_freq = 10000 # DQN: 10000
 
 grad_scaling_methods = ['none','full']
 grad_scaling_method = grad_scaling_methods[1]
 
 # Random noise is added to the loss of the SNF during training of the opt net.
-loss_noise = 0.25
+loss_noise = 0.75
--- v3/main.py
+++ v3/main.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from constants import num_iterations, seq_length, save_path, summary_freq, \
     episode_length, replay_mem_start_size, replay_memory_max_size, \
-	num_SNFs, num_rnn_layers, rnn_size, m
+	num_SNFs, num_rnn_layers, rnn_size, m, net_sync_freq
 from snf import SNF, State, StateOps
 from optimizer import Optimizer
 
@@ -32,7 +32,12 @@ def main():
 	sess = tf.Session()
 		
 	state_ops = StateOps()
-	opt_net = Optimizer()
+	
+	with tf.variable_scope("opt1"):
+		opt_net = Optimizer()
+		
+	with tf.variable_scope("opt2"):
+		opt_net2 = Optimizer() # Only used for generating new states
 	
 	snfs = []
 	# Generate the set of SNFs
@@ -63,46 +68,70 @@ def main():
 		state = random.choice(replay_memory)
 		snf = state.snf
 		
-		for j in range(seq_length):
-			feed_dict = {opt_net.point: state.point, 
-							opt_net.snf_loss: [state.loss],
-							opt_net.variances: snf.variances, 
-							opt_net.weights: snf.weights, 
-							opt_net.hyperplanes: snf.hyperplanes, 
-							opt_net.input_grads: state.grads,
-							opt_net.step_size: np.ones([m]),
-							opt_net.initial_rnn_state: state.rnn_state,
-							opt_net.state_index: state.counter}
-							
-			loss, snf_loss, new_point, rnn_state, grads,_ = sess.run([opt_net.loss, 
-														opt_net.new_snf_loss,
-														opt_net.new_point, 
-														opt_net.rnn_state, 
-														opt_net.grads, 
-														opt_net.train_step], 
-														feed_dict=feed_dict)
-			losses.append(loss)
+		#for j in range(seq_length): ### necessary? causes issues from correlation?
+		#===# Generate a new state #===#
+		feed_dict = {opt_net2.point: state.point, 
+						opt_net2.snf_loss: [state.loss],
+						opt_net2.variances: snf.variances, 
+						opt_net2.weights: snf.weights, 
+						opt_net2.hyperplanes: snf.hyperplanes, 
+						opt_net2.input_grads: state.grads,
+						opt_net2.step_size: np.ones([m]),
+						opt_net2.initial_rnn_state: state.rnn_state,
+						opt_net2.state_index: state.counter}
+						
+		snf_loss, new_point, rnn_state, grads = sess.run([opt_net2.new_snf_loss,
+													opt_net2.new_point, 
+													opt_net2.rnn_state, 
+													opt_net2.grads], 
+													feed_dict=feed_dict)
+		
+		new_state = state
+		new_state.loss = snf_loss
+		new_state.point = new_point
+		new_state.rnn_state = rnn_state
+		new_state.grads = grads
+		
+		new_state.counter += 1
+		if new_state.counter >= episode_length:
+			snf = random.choice(snfs)
+			new_state = State(snf, state_ops, sess)
+		
+		replay_memory.append(new_state)
+		
+		#===# Train the optimizer #===#
+		state = random.choice(replay_memory)
+		snf = state.snf	
+		
+		feed_dict = {opt_net.point: state.point, 
+						opt_net.snf_loss: [state.loss],
+						opt_net.variances: snf.variances, 
+						opt_net.weights: snf.weights, 
+						opt_net.hyperplanes: snf.hyperplanes, 
+						opt_net.input_grads: state.grads,
+						opt_net.step_size: np.ones([m]),
+						opt_net.initial_rnn_state: state.rnn_state,
+						opt_net.state_index: state.counter}
+						
+		loss,_ = sess.run([opt_net.loss, opt_net.train_step], feed_dict=feed_dict)
+		losses.append(loss)		
+		
+		# Synchronize optimizers
+		if i & net_sync_freq == 0 and i > 0:
+			opt1_vars = [j for j in tf.trainable_variables() if 'opt1' in j.name]
+			opt2_vars = [j for j in tf.trainable_variables() if 'opt2' in j.name]
+			for v1,v2 in zip(opt1_vars,opt2_vars):
+				# The net which generates states copies the variables of the net which is trained.
+				v2.assign(v1)
 			
-			new_state = state
-			new_state.loss = snf_loss
-			new_state.point = new_point
-			new_state.rnn_state = rnn_state
-			new_state.grads = grads
-			
-			new_state.counter += 1
-			if new_state.counter >= episode_length:
-				snf = random.choice(snfs)
-				new_state = State(snf, state_ops, sess)
-			
-			replay_memory.append(new_state)
-			
-		if i % summary_freq == 0:
+		if i % summary_freq == 0 and i > 0:
 			print "%d\t%.5f" % (i, np.mean(losses))
 			losses = []
 			
 	# Save model
 	if args.save_model:
-		saver = tf.train.Saver(tf.trainable_variables())
+		vars_to_save = [j for j in tf.trainable_variables() if 'opt2' in j.name]
+		saver = tf.train.Saver(vars_to_save)
 		saver.save(sess, save_path)
 		print "Model saved"
 
